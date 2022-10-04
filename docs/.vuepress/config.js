7f7152e3fe2830a87715ce71a0451f5fcfaf210b
Kaikaikaifang <2837968358@qq.com>
2022-10-04 20:23:25 +0800

    init: kaikai's blog

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,82 @@
 // 一个基础的配置文件
 import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '@vuepress/theme-default'
+
 
 export default defineUserConfig({
   lang: 'zh-CN',
   title: "Kaikai's Blog",
   description: 'Kaikai 的个人博客',
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+    },
+    '/en/': {
+      lang: 'en-US',
+    },
+  },
+  theme: defaultTheme({
+    // 在这里进行配置
+    navbar: [
+      // 嵌套 Group - 最大深度为 2
+      {
+        text: 'Group',
+        children: [
+          {
+            text: 'SubGroup',
+            children: ['/group/sub/foo.md', '/group/sub/bar.md'],
+          },
+        ],
+      },
+      // 控制元素何时被激活
+      {
+        text: 'Group 2',
+        children: [
+          {
+            text: 'Always active',
+            link: '/',
+            // 该元素将一直处于激活状态
+            activeMatch: '/',
+          },
+          {
+            text: 'Active on /foo/',
+            link: '/not-foo/',
+            // 该元素在当前路由路径是 /foo/ 开头时激活
+            // 支持正则表达式
+            activeMatch: '^/foo/',
+          },
+        ],
+      },
+    ],
+    logo: '/images/logo.jpg',
+    logoDark: '/images/logo_dark.jpg',
+    repo: 'https://github.com/Kaikaikaifang/Kaikai-s-Blog',
+    locales: {
+      '/': {
+        selectLanguageName: '简体中文',
+      },
+      '/en/': {
+        selectLanguageName: 'English',
+      },
+    },
+    // sidebar: 'auto'
+    sidebar: {
+      '/': [
+        {
+          text: 'VuePress Reference',
+          collapsible: true,
+          children: ['/reference/cli.md', '/reference/config.md'],
+        },
+        {
+          text: 'Bundlers Reference',
+          collapsible: true,
+          children: ['/reference/bundler/vite.md', '/reference/bundler/webpack.md'],
+        },
+      ],
+    },
+    sidebarDepth: 2,   //  number default: 2
+    editLink: true,
+    editLinkText: 'Edit this page',
+  }),
 })
 
--- docs/README.md
+++ docs/README.md
@@ -3,3 +3,4 @@ lang: zh-CN
 title: Kaikai's Blog
 description: Kaikai 的个人博客
 ---
+![Logo](/images/hello_world.png)
